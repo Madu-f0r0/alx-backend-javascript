9788c55959e912b959e6e8020b179d9fbc72e071
Madu-f0r0 <duherbert@gmail.com>
2024-04-30 12:17:09 +0100

    Script 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
+  console.log(`Your name is: ${data}`);
   process.exit();
 });
 
