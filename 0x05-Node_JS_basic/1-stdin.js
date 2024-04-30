0770de017d65e171bcc9b4a4ea7c671169196377
Madu-f0r0 <duherbert@gmail.com>
2024-04-30 13:04:07 +0100

    Script 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,12 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
   process.exit();
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (!process.stdin.isTTY) {
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
