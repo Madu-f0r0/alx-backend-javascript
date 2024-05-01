b62ef21b6bb832e3505577bb08bf1f8379115cf2
Madu-f0r0 <duherbert@gmail.com>
2024-05-01 18:23:29 +0100

    My first JS unittests using mocha

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./0-calcul.js');
+
+describe('rounded', () => {
+  it('both args are rounded', () => {
+    const sum = calculateNumber(3, 4);
+    assert.strictEqual(sum, 7);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(3, 0);
+    assert.strictEqual(sum, 3);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(0, 0);
+    assert.strictEqual(sum, 0);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(-1, 0);
+    assert.strictEqual(sum, -1);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(-1, 2);
+    assert.strictEqual(sum, 1);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(-1, -2);
+    assert.strictEqual(sum, -3);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(1.5, 2.4);
+    assert.strictEqual(sum, 4);
+  });
+});
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./1-calcul.js');
+
+describe('tests for sum', () => {
+  it ('zero arguments', () => {
+    assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+    assert.strictEqual(calculateNumber('SUM', 1, 0), 1);
+    assert.strictEqual(calculateNumber('SUM', 0, 2), 2);
+  });
+  it ('floats are rounded', () => {
+    assert.strictEqual(calculateNumber('SUM', 1.5, 2.4), 4);
+    assert.strictEqual(calculateNumber('SUM', 1.5, 3), 5);
+    assert.strictEqual(calculateNumber('SUM', 3, 2.7), 6);
+  });
+  it ('keeps integers intact', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 2), 3);
+  });
+});
+
+describe('tests for subtract', () => {
+  it ('zero arguments', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1, 0), 1);
+    assert.strictEqual(calculateNumber('SUBTRACT', 0, 2), -2);
+  });
+  it ('floats are rounded', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 2.4, 1.5), 0);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3), -1);
+    assert.strictEqual(calculateNumber('SUBTRACT', 3, 2.7), 0);
+  });
+  it ('keeps integers intact', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 2, 1), 1);
+  });
+});
+
+describe('tests for divide', () => {
+    it ('zero arguments', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 5, 0), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', 1, 0.4), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', 0, 2), 0);
+    });
+    it ('floats are rounded', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 2.4, 1.5), 1);
+      assert.strictEqual(calculateNumber('DIVIDE', 3.5, 2), 2);
+      assert.strictEqual(calculateNumber('DIVIDE', 2, 3.7), 0.5);
+    });
+    it ('keeps integers intact', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 2, 1), 2);
+    });
+  });
