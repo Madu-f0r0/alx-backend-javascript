c02e610466080cb31d479ce0632f1f3a6c2e890d
Madu-f0r0 <duherbert@gmail.com>
2024-05-01 18:52:45 +0100

    Added tests for 0-calcul module

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,14 @@
 const assert = require('assert');
-const { it, describe } = require('mocha');
 const calculateNumber = require('./0-calcul.js');
 
-describe('rounded', () => {
+describe('calculateNumber', () => {
   it('both args are rounded', () => {
     const sum = calculateNumber(3, 4);
     assert.strictEqual(sum, 7);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(0, 3);
+    assert.strictEqual(sum, 3);
   });
   it('both args are rounded', () => {
     const sum = calculateNumber(3, 0);
@@ -18,6 +21,10 @@ describe('rounded', () => {
   it('both args are rounded', () => {
     const sum = calculateNumber(-1, 0);
     assert.strictEqual(sum, -1);
+  });
+  it('both args are rounded', () => {
+    const sum = calculateNumber(0, -2);
+    assert.strictEqual(sum, -2);
   });
   it('both args are rounded', () => {
     const sum = calculateNumber(-1, 2);
