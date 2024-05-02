e23d16c2914ae6361ef625737433ccb9b1bdc142
Madu-f0r0 <duherbert@gmail.com>
2024-05-02 12:56:52 +0100

    Added script 3 and tests for script 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,4 +1,5 @@
-const { assert } = require('chai');
+const assert = require('assert');
+const sinon = require("sinon");
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
