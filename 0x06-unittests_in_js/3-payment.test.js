de11a337fbcca390e68ec5f35c3943dc0cb429af
Madu-f0r0 <duherbert@gmail.com>
2024-05-02 12:45:42 +0100

    Added script 3 and tests for script 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,12 @@
+const { assert } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('gives same result as when Utils.calculateNumber is used', () => {
+    const spiedFunction = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    assert(spiedFunction.calledOnce);
+    spiedFunction.restore();
+  });
+});
